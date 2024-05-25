50b4c3400aebc7c01b5daf862ab56c554c2a3368
Jesse Conner <me@jesseconner.ca>
2024-05-25 17:29:43 -0400

    Formatting

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,14 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Item extends Model {
+class Item extends Model
+{
     protected $fillable = [ 'name', 'user_id' ];
 
-    public function user(): BelongsTo {
-        return $this->belongsTo( User::class );
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 
-    public function aisleItems(): HasMany {
-        return $this->hasMany( AisleItem::class );
+    public function aisleItems(): HasMany
+    {
+        return $this->hasMany(AisleItem::class);
     }
 }
--- app/Models/Store.php
+++ app/Models/Store.php
@@ -6,13 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Store extends Model {
-    public function aisles(): HasMany {
-        return $this->hasMany( Aisle::class );
+class Store extends Model
+{
+    public function aisles(): HasMany
+    {
+        return $this->hasMany(Aisle::class);
     }
 
-    public function user(): BelongsTo {
-        return $this->belongsTo( User::class );
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 
     protected $fillable = [ 'name', 'user_id' ];
