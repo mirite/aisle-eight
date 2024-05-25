1d1f953290cc53e072adc133c0a8098932a5e384
Jesse Conner <me@jesseconner.ca>
2024-05-25 09:59:01 -0400

    Switch grocery list title to not be nullable. Attempting to fix #4

---
+++ database/migrations/2024_05_25_133714_add_grocery_list_title.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+
+        Schema::table('grocery_lists', function (Blueprint $table) {
+            if(!Schema::hasColumn('grocery_lists', 'title')) {
+                $table->string('title')->default( '' );;
+            }
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+
+
+    }
+};
