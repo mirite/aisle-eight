09f2a51b56b7e3dcfb0d68cbf84a573489904fc5
Jesse Conner <me@jesseconner.ca>
2024-05-31 20:53:03 -0400

    Formatting

--- app/Http/Controllers/GroceryListController.php
+++ app/Http/Controllers/GroceryListController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class GroceryListController extends Controller
 {
-    public static function getNewestListId() {
+    public static function getNewestListId()
+    {
         return auth()->user()->groceryLists()->latest()->first()->id;
     }
 
