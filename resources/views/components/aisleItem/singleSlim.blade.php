fe6d1e34dfb3bcb1a6d0c3f99db60af8a66a8cc0
Jesse Conner <me@jesseconner.ca>
2024-06-08 19:13:44 -0400

    Add prices to shopping screen. Closes #18

--- resources/views/components/aisleItem/singleSlim.blade.php
+++ resources/views/components/aisleItem/singleSlim.blade.php
@@ -1,2 +1,9 @@
-<span>{{ $item->item->name }} {{ $showStore ?? true ? $item->aisle->store->name : '' }} {{ $item->price }}
-    {{ $item->description }}</span>
+<div class="flex gap-2 flex-col md:flex-row"><span>{{ $item->item->name }}</span>
+    @if ($showStore ?? true)
+        <span>{{ $item->aisle->store->name }}</span>
+    @endif
+    @if ($showPrice ?? true)
+        <span>{{ $item->price }}</span>
+    @endif
+    <span>{{ $item->description }}</span>
+</div>
