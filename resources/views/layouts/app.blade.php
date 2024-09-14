530c9ac868614bc7699a988fbb603d52b29a77c8
Jesse Conner <me@jesseconner.ca>
2024-09-14 09:17:55 -0400

    Layout tweaks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,35 +5,20 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Aisle Eight') }}</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-dvh bg-white-100 text-bg-900 dark:text-white dark:bg-slate-900">
-        <livewire:layout.navigation />
-
-        <!-- Page Heading -->
-        @if (isset($header))
-            <header class="bg-white dark:bg-slate-950 shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endif
-
-        <!-- Page Content -->
-        <main class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-            {{ $slot }}
-        </main>
-    </div>
+<body
+    class="font-sans antialiased m-0 min-h-dvh bg-white-100 text-bg-900 dark:text-white dark:bg-slate-900 flex flex-col gap-2">
+    <livewire:layout.navigation />
+    <main class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 w-full grow">
+        {{ $slot }}
+    </main>
+    <footer class="mx-auto text-center max-w-screen-2xl w-full p-4">
+        <a href="https://jesseconner.ca" target="_blank">&copy;Jesse Conner {{ date('Y') }}</a>
+    </footer>
 </body>
 
 </html>
